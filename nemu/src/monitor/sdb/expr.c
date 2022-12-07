87921d4e3b06d46adb14e23f741f1d50a70829a4
Dongdong Zhang <zhangdongdong@eswincomputing.com>
2022-12-07 19:12:55 +0800

    fix: add token to tokens array

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -24,6 +24,8 @@ enum {
     TK_NOTYPE = 256,
     TK_EQ,
     TK_HEX,
+    TK_NUM,
+    TK_REG,
 
     /* TODO: Add more token types */
 
@@ -42,10 +44,16 @@ static struct rule {
    * 注意不同规则的优先级。
    */
 
-    { " +", TK_NOTYPE },             // spaces
-    { "0[xX][0-9a-fA-F]+", TK_HEX }, // hex
-    { "\\+", '+' },                  // plus
-    { "==", TK_EQ },                 // equal
+    { " +", TK_NOTYPE },               // spaces
+    { "==", TK_EQ },                   // equal
+    { "0x[0-9a-fA-F]{1,16}", TK_HEX }, // hex
+    { "[0-9]{1,10}", TK_NUM },         // dec
+    { "\\$[a-z0-9]{1,31}", TK_REG },   // register names
+    { "\\+", '+' },                    // +
+    { "-", '-' },                      // -
+    { "\\*", '*' },                    // *
+    { "/", '/' },                      // /
+    { "%", '%' }
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -79,12 +87,17 @@ typedef struct token {
 static Token tokens[32] __attribute__((used)) = {};
 static int   nr_token __attribute__((used))   = 0;
 
+/**
+ * token 就是表达式中的各个元素，比如3+2中的token胡原始股'3' '+' '2' 
+ * 给出一个待求值表达式, 我们首先要识别出其中的token, 进行这项工作的是make_token()函数
+ */
 static bool make_token(char *e)
 {
     int        position = 0;
     int        i;
     regmatch_t pmatch;
 
+    /* 表示已经被识别出来的token数量 */
     nr_token = 0;
 
     while (e[position] != '\0') {
@@ -98,7 +111,7 @@ static bool make_token(char *e)
                 Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
                     i, rules[i].regex, position, substr_len, substr_len,
                     substr_start);
-
+                /* 每个token都有一定长度需要记录当前token有多长 */
                 position += substr_len;
 
                 /**
@@ -108,7 +121,7 @@ static bool make_token(char *e)
                  */
                 /**
                  * TODO：现在一个新的 token 被 rules[i] 识别。 
-                 * 添加代码以将令牌记录在数组“令牌”中。 对于某些类型的令牌，应该执行一些额外的操作。
+                 * 添加代码以将tookens记录在数组“tookens”中。 对于某些类型的tookens，应该执行一些额外的操作。对于非运算符的token，还需要记录下它的内容
                  */
 
                 switch (rules[i].token_type) {
